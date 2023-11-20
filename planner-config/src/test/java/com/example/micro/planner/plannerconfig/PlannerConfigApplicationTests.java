505dd7b7be871ed701533bb4827cc18103205235
ivan.belyaev <belivan84@gmail.com>
2023-11-21 00:20:34 +0300

    подключение eureka server и config server

---
+++ planner-config/src/test/java/com/example/micro/planner/plannerconfig/PlannerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.micro.planner.plannerconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ planner-eureka-server/src/test/java/com/example/micro/planner/plannereurekaserver/PlannerEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.micro.planner.plannereurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
