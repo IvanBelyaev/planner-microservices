37a2020f922cb51b1f58f742c19f95b1ff37e6fb
ivan.belyaev <belivan84@gmail.com>
2023-11-22 01:20:30 +0300

    добавление микросервиса для работы с задачами

---
+++ planner-todo/src/main/java/com/example/micro/planner/todo/PlannerTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.micro.planner.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerTodoApplication.class, args);
+	}
+
+}
---
+++ planner-todo/src/test/java/com/example/micro/planner/todo/PlannerTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.micro.planner.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
